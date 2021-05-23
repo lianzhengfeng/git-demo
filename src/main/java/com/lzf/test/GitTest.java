622bf52a572bf4406a66c94c19be152391aec86e
lianzhengfeng <782569800@qq.com>
2021-05-23 22:48:19 +0800

    first commit

---
+++ src/main/java/com/lzf/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.lzf.test;
+
+/**
+ * @author lianzhengfeng
+ * @create 2021-05-23-22:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
